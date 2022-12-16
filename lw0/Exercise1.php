f1b7b1415eefbef72a9d1761fb6dc537f5eccab4
Evgeniya-just <113514729+Evgeniya-just@users.noreply.github.com>
2022-12-17 02:58:14 +0300

    feat(lw0 Exercise1):create exercise1

---
+++ lw0/Exercise1.php
@@ -0,0 +1,22 @@
+<?php
+
+function calculator($str)
+{
+    preg_match_all('!\d+!', $str, $matches);
+
+    if (count($matches[0]) > 5)
+        return "Количество слагаемых не должно быть больше 5!";
+    if (!is_string($str))
+        return "Параметр должен быть типа string!";
+    $pattern = '#^[0-9+-]+$#';
+    $error = "Incorrect input";
+    if (!preg_match($pattern, $str))
+        return $error;
+    $p = eval('return ' . $str . ';');
+    return $p;
+}
+
+echo calculator('1+1');
+echo calculator('1+1-2');
+echo calculator('4+1-2+3');
+echo calculator('str');
---
+++ lw0/Exercise2.php
@@ -0,0 +1,23 @@
+<?php
+
+function sumTime($firstTime, $secondTime)
+{
+    if ((!is_string($firstTime))  or (!is_string($firstTime))) return " Оба передаваемых параметра должны быть строками!";
+    $firstTime = trim($firstTime);
+    $secondTime = trim($secondTime);
+
+    $pattern = '#^[0-2]*[0-4]:[0-5]*[0-9]:[0-5]*[0-9]$#';
+    $error = "Формат времени должен быть: 00:00:00 - 24:59:59 и состоять только из цифр и двоеточий!";
+
+    if (!preg_match($pattern, $firstTime)) return $error;
+    if (!preg_match($pattern, $secondTime)) return $error;
+
+    $list = explode(":", $secondTime);
+    $SumDate = date('H:i:s', strtotime($firstTime . " +$list[0] hours $list[1] minutes $list[2] seconds"));
+    return $SumDate;
+}
+
+echo sumTime('10:20:30', '10:20:30');
+echo sumTime('10:30:30', '10:30:30');
+echo sumTime('20:30:30', '20:30:30');
+echo sumTime('r0:30:30', '20:z0:30');
